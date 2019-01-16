5c9705ce098673e82847570657e3aa981b9433bf
Cuong Duy Nguyen <ndc07.it@gmail.com>
2019-01-16 13:07:48 +0700

    using template engines with pug

--- app.js
+++ app.js
@@ -2,5 +2,16 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello Express'))
+app.set('view engine', 'pug')
+app.set('views', './views')
+
+app.get('/', (req, res) => res.render('index', { screenName: 'Home' }))
+app.get('/users', (req, res) => res.render('users/index', { 
+	screenName: 'Users',
+	users: [
+		{id: 1, name: 'jade28'},
+		{id: 2, name: 'iamzeng'}
+		]
+}))
+
 app.listen(port, () => console.log(`Listening on port ${port}`))
