d80c63e0e61a3ba7364d98a79a997bf9edd6e856
Cuong Duy Nguyen <ndc07.it@gmail.com>
2019-01-17 13:58:21 +0700

    POST method

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
 const port = 3000
 
@@ -11,6 +12,9 @@ const users = [
 
 app.set('view engine', 'pug')
 app.set('views', './views')
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => res.render('index', { screenName: 'Home' }))
 
@@ -28,6 +32,15 @@ app.get('/users/search', (req, res) => {
 		users: mathchedUsers,
 		q: q
 	})
+})
+
+app.get('/users/create', (req, res) => {
+	res.render('users/create', { screenName: 'Create User' })
+})
+
+app.post('/users/create', (req, res) => {
+	users.push(req.body)
+	res.redirect('/users')
 })
 
 app.listen(port, () => console.log(`Listening on port ${port}`))
