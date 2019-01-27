7bf106ea3dfa57efdc4b6c8d2fa17b1c8f8f2a66
Cuong Duy Nguyen <ndc07.it@gmail.com>
2019-01-28 00:06:19 +0700

    Environment Variables - using 🔴dotenv

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+require('dotenv').config()
+
 const express = require('express')
 const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser')
@@ -17,7 +19,7 @@ app.set('views', './views')
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cookieParser('h8u7iow0xmwql'))
+app.use(cookieParser(process.env.SESSION_SECRET))
 
 app.use(express.static('public'))
 
