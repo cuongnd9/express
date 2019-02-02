1d7c7a7b7f49af19280b70f2c3746374c7d05bc8
Cuong Duy Nguyen <ndc07.it@gmail.com>
2019-02-02 15:29:14 +0700

    pagination with Bootstrap 4

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -5,8 +5,31 @@ module.exports.index = (req, res) => {
 	const perPage = 12
 	const start = (page - 1) * perPage
 	const end = page * perPage
+
+	const pages = []
+	const totalItems = db.get('products').value().length
+	const lastPage = Math.ceil(totalItems / perPage)
+	var disabledPage = 0 // 0: not, -1: previous, 1: next
+
+	if (page === 1) {
+		pages.push(1, 2 , 3)
+		disabledPage = -1
+	}
+
+	if (page === lastPage) {
+		pages.push(page - 2, page - 1, page)
+		disabledPage = 1
+	}
+
+	if (page > 1 && page < lastPage) {
+		pages.push(page - 1, page, page + 1)
+	}
+
 	res.render('products/index', {
 		title: 'Products',
-		products: db.get('products').value().slice(start, end)
+		products: db.get('products').value().slice(start, end),
+		pages: pages,
+		activePage: page,
+		disabledPage: disabledPage
 	})
 }
